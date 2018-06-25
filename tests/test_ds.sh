ea31b1d4ee18b6ac2aa3bcf194ce8359993bc1ba
Denny Zhang <denny.zhang001@gmail.com>
2018-06-25 11:52:13 -0700

    improve the test logic

--- tests/test_ds.sh
+++ tests/test_ds.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 ##-------------------------------------------------------------------
 ##
 ## File : test_ds.sh
@@ -6,17 +6,22 @@
 ## Description :
 ## --
 ## Created : <2017-08-15>
-## Updated: Time-stamp: <2018-06-25 11:30:47>
+## Updated: Time-stamp: <2018-06-25 11:51:36>
 ##-------------------------------------------------------------------
 function setup_daemonset() {
     echo "Setup Daemonset"
+    kubectl apply -f configmap/fluent-bit-configmap.yml
+    kubectl apply -f daemonsets/daemonset.yaml
 }
 
 function verify_daemonset() {
     echo "Verify Daemonset deployment"
-    kubectl get daemonsets -n oratos
-    kubectl get pods -n oratos
+    kubectl get daemonsets -n oratos | grep fluent-bit
+    kubectl get pods -n oratos | grep fluent-bit.*Running
 }
 
-kubectl get daemonsets --namespace oratos
+cd ..
+
+setup_daemonset
+verify_daemonset
 ## File : test_ds.sh ends
