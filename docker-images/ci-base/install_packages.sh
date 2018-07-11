5750e2658e2a64e2aa92c0ba1cff5830058731bc
Jason Keene <jkeene@pivotal.io>
2018-07-11 14:07:56 -0600

    Reduce the size of the ci-base image
    Signed-off-by: Warren Fernandes <wfernandes@pivotal.io>

---
+++ docker-images/ci-base/install_packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt-get update
+apt-get install --yes \
+        jq \
+        rsync \
+        unzip \
+        psmisc \
+        vim \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg2 \
+        software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-get install --yes docker-ce
+
+echo "deb http://packages.cloud.google.com/apt cloud-sdk-$(lsb_release -c -s) main" | \
+    tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+apt-get update
+apt-get install --yes google-cloud-sdk
+
+rm -rf /var/lib/apt/lists/*
