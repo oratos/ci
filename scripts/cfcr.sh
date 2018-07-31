aee4f9cebf54f44f7a20531e593aba17cd88cd26
Tom Chen <tochen@pivotal.io>
2018-07-31 11:07:48 -0600

    Make generic cfcr env script from bikepark script
    Signed-off-by: Jason Keene <jkeene@pivotal.io>

--- scripts/cfcr.sh
+++ scripts/cfcr.sh
@@ -2,12 +2,10 @@
 
 set -Eeo pipefail; [ -n "$DEBUG" ] && set -x; set -u
 
-BIKEPARK_BBL_STATE_DIR="${BIKEPARK_BBL_STATE_DIR:-$HOME/workspace/bikepark-bbl-state}"
-
-function bikepark_download-bbl-state {
-    mkdir -p "$BIKEPARK_BBL_STATE_DIR"
-    pushd "$BIKEPARK_BBL_STATE_DIR" > /dev/null
-        vault read -format json secret/envs/bikepark-bbl-state \
+function cfcr_env_download-bbl-state {
+    mkdir -p "$BBL_STATE_DIR"
+    pushd "$BBL_STATE_DIR" > /dev/null
+        vault read -format json "$vault_path" \
             | jq --join-output .data.tarball \
             | base64 --decode \
             > bbl-state.tgz
@@ -15,70 +13,70 @@ function bikepark_download-bbl-state {
     popd > /dev/null
 }
 
-function bikepark_print-env {
-    pushd "$BIKEPARK_BBL_STATE_DIR/bbl-state" > /dev/null
+function cfcr_env_print-env {
+    pushd "$BBL_STATE_DIR/bbl-state" > /dev/null
         bbl print-env
     popd > /dev/null
 }
 
-function bikepark_delete-bbl-state {
-    rm -r "$BIKEPARK_BBL_STATE_DIR"
+function cfcr_env_delete-bbl-state {
+    rm -r "$BBL_STATE_DIR"
 }
 
 function cleanup_bbl_state {
     echo "unable to download bbl-state"
     echo "perhaps you are not logged into vault?"
     echo "removing bbl state"
-    rm -r "$BIKEPARK_BBL_STATE_DIR"
+    rm -r "$BBL_STATE_DIR"
     exit 1
 }
 
-function bikepark_get-credentials {
-    if ! [ -d "$BIKEPARK_BBL_STATE_DIR" ]; then
+function cfcr_env_get-credentials {
+    if ! [ -d "$BBL_STATE_DIR" ]; then
         echo "downloading state from vault..."
         echo "this may take a while but you only have to do it once"
 
         trap cleanup_bbl_state ERR
-        bikepark_download-bbl-state
+        cfcr_env_download-bbl-state
         trap '' ERR
     fi
-    eval "$(bikepark_print-env)"
+    eval "$(cfcr_env_print-env)"
     admin_password_key=$(credhub find --name-like /cfcr/kubo-admin-password --output-json | jq --raw-output ".credentials[0].name")
     admin_password=$(credhub get --name "$admin_password_key" --output-json | jq --raw-output .value)
 
-    kubectl config set-cluster bikepark \
-      --server="https://bikepark.oratos.ci.cf-app.com" \
+    kubectl config set-cluster "$env" \
+      --server="https://$domain" \
       --insecure-skip-tls-verify=true
-    kubectl config set-credentials bikepark-admin --token="$admin_password"
-    kubectl config set-context bikepark \
-      --cluster=bikepark \
-      --user=bikepark-admin \
+    kubectl config set-credentials "$env-admin" --token="$admin_password"
+    kubectl config set-context "$env" \
+      --cluster="$env" \
+      --user="$env-admin" \
       --namespace=oratos
-    kubectl config use-context bikepark
+    kubectl config use-context "$env"
 }
 
-function bikepark_get-credentials-tunnel {
-    if ! [ -d "$BIKEPARK_BBL_STATE_DIR" ]; then
+function cfcr_env_get-credentials-tunnel {
+    if ! [ -d "$BBL_STATE_DIR" ]; then
         echo "downloading state from vault..."
         echo "this may take a while but you only have to do it once"
 
         trap cleanup_bbl_state ERR
-        bikepark_download-bbl-state
+        cfcr_env_download-bbl-state
         trap '' ERR
     fi
-    eval "$(bikepark_print-env)"
+    eval "$(cfcr_env_print-env)"
     admin_password_key=$(credhub find --name-like /cfcr/kubo-admin-password --output-json | jq --raw-output ".credentials[0].name")
     admin_password=$(credhub get --name "$admin_password_key" --output-json | jq --raw-output .value)
 
-    kubectl config set-cluster bikepark \
+    kubectl config set-cluster "$env" \
       --server="https://localhost:8443" \
       --insecure-skip-tls-verify=true
-    kubectl config set-credentials bikepark-admin --token="$admin_password"
-    kubectl config set-context bikepark \
-      --cluster=bikepark \
-      --user=bikepark-admin \
+    kubectl config set-credentials "$env-admin" --token="$admin_password"
+    kubectl config set-context "$env" \
+      --cluster="$env" \
+      --user="$env-admin" \
       --namespace=oratos
-    kubectl config use-context bikepark
+    kubectl config use-context "$env"
 
     export BOSH_DEPLOYMENT=cfcr
     master_host=$(
@@ -86,7 +84,7 @@ function bikepark_get-credentials-tunnel {
             | jq '.Tables[].Rows[] | select(.instance | contains("master"))' \
             | jq --slurp --join-output first.ips
     )
-    pushd "$BIKEPARK_BBL_STATE_DIR/bbl-state" > /dev/null
+    pushd "$BBL_STATE_DIR/bbl-state" > /dev/null
         jumpbox_host="$(bbl jumpbox-address)"
         jumpbox_key="$(bbl ssh-key)"
     popd > /dev/null
@@ -108,7 +106,7 @@ function bikepark_get-credentials-tunnel {
 
 function print_usage {
     bn="$(basename $0)"
-    echo "$bn: [subcommand]"
+    echo "$bn: <environment> <subcommand>"
     echo
     echo -e "\033[1mSubcommands:\033[0m"
     echo "   get-credentials         set kubernetes context"
@@ -120,18 +118,42 @@ function print_usage {
     echo "   download-bbl-state  download bbl state from vault"
 }
 
-function parse_argc {
-    arg=${1:-}
-    case "$arg" in
+function set_globals {
+    env=${1:-}
+    case "$env" in
+        bikepark)
+            domain=bikepark.oratos.ci.cf-app.com
+            vault_path=secret/envs/bikepark-bbl-state
+            ;;
+        testing)
+            domain=testing-cfcr.oratos.ci.cf-app.com
+            vault_path=secret/envs/oratos-ci-testing-cfcr-bbl-state
+            ;;
+        "")
+            print_usage
+            exit 0
+            ;;
+        *)
+            echo -e "Invalid environment: $env\n"
+            print_usage
+            exit 1
+            ;;
+    esac
+
+    BBL_STATE_DIR="${BBL_STATE_DIR:-$HOME/workspace/$env-bbl-state}"
+
+    local cmd
+    cmd=${2:-}
+    case "$cmd" in
+        print-env|delete-bbl-state|download-bbl-state|get-credentials|get-credentials-tunnel)
+            command=cfcr_env_$cmd
+            ;;
         -h|-help|--help|help)
             print_usage
             exit 0
             ;;
-        print-env|delete-bbl-state|download-bbl-state|get-credentials|get-credentials-tunnel)
-            command=bikepark_$arg
-            ;;
         *)
-            echo -e "Invalid command: $arg\n"
+            echo -e "Invalid command: $cmd\n"
             print_usage
             exit 1
             ;;
@@ -139,7 +161,8 @@ function parse_argc {
 }
 
 function main {
-    parse_argc ${1:-}
+    set_globals $@
+    shift
     shift
     "$command" $@
 }
