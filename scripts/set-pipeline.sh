4e195f03b113e230fbac5191d1206a5d02322f2d
Jason Keene <jkeene@pivotal.io>
2018-10-04 14:48:37 -0600

    Add shellcheck and fixes task bash scripts
    Signed-off-by: Warren Fernandes <wfernandes@pivotal.io>

--- scripts/set-pipeline.sh
+++ scripts/set-pipeline.sh
@@ -6,22 +6,25 @@ function set_globals {
     TARGET="${TARGET:-oratos}"
 }
 
-function validate {
+function validate_args {
     if [ "$pipeline" = "-h" ] || [ "$pipeline" = "--help" ]; then
         print_usage
         exit 1
     fi
 }
 
+function shellcheck_tasks {
+    for task in tasks/**/task; do
+       shellcheck "$task"
+    done
+}
+
 function set_pipeline {
     echo setting pipeline for "$1"
     fly -t "$TARGET" set-pipeline -p "$1" \
         -l pipelines/vars/global.yml \
-        -c <(
-        cat "pipelines/$1.yml" \
-            | yq read - --tojson \
-            | jq '.jobs[].build_logs_to_retain = 20'
-    )
+        -c <(yq read "pipelines/$1.yml" --tojson \
+                | jq '.jobs[].build_logs_to_retain = 20')
 }
 
 function sync_fly {
@@ -30,7 +33,8 @@ function sync_fly {
 
 function set_pipelines {
     if [ "$pipeline" = all ] || [ -z "$pipeline" ]; then
-        for pipeline_file in $(ls pipelines/*.yml | sed 's|^pipelines/||'); do
+        for pipeline_file in $(find pipelines -depth 1 -iname "*.yml" \
+                                | sed 's|^pipelines/||'); do
             set_pipeline "${pipeline_file%.yml}"
         done
         exit 0
@@ -43,12 +47,30 @@ function print_usage {
     echo "usage: $0 <pipeline>"
 }
 
+function verify_dependencies {
+    local failed=false
+    local deps=( shellcheck fly yq jq )
+
+    for d in "${deps[@]}"; do
+        if ! command -v "$d" > /dev/null 2>&1; then
+            echo "$d required"
+            failed=true
+        fi
+    done
+
+    if [ "$failed" != false ]; then
+        exit 1
+    fi
+}
+
 function main {
     local pipeline="${1:-}"
 
+    verify_dependencies
     set_globals "$pipeline"
-    validate
+    validate_args
     sync_fly
+    shellcheck_tasks
     set_pipelines
 }
-main $@
+main "$@"
