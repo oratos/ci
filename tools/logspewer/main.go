1b2bb71b7714d2122a55bb9c1f73cd9673983cf2
David Timm <dtimm@pivotal.io>
2019-10-04 10:29:17 -0600

    Adds ability to set custom delay for logspewer also makes argyle print nicer
    Signed-off-by: Rachel Heaton <rheaton@pivotal.io>
    Co-authored-by: Rachel Heaton <rheaton@pivotal.io>

--- tools/logspewer/main.go
+++ tools/logspewer/main.go
@@ -9,15 +9,26 @@ import (
 
 func main() {
 	msg := os.Getenv("MESSAGE")
+	messageDelay := os.Getenv("MESSAGE_DELAY")
+	var delay = time.Second
+
 	if len(msg) == 0 {
 		msg = "Log Message"
+	}
+
+	if len(messageDelay) != 0 {
+		var err error
+		delay, err = time.ParseDuration(messageDelay)
+		if err != nil {
+			log.Fatalf("Could not parse MESSAGE_DELAY: %s", err)
+		}
 	}
 
 	count := 0
 	for {
 		log.Printf("%s %s %d\n", ponger(count), msg, count)
 		count++
-		time.Sleep(time.Millisecond * 100)
+		time.Sleep(delay)
 	}
 }
 
@@ -25,11 +36,15 @@ const width int = 41
 
 func ponger(i int) string {
 	text := []rune("[" + strings.Repeat("-", width) + "]")
+
+	text[(i+width/2)%width+1] = '*'
+	text[width-(i+width/2)%width] = '*'
 	if i%width+1 == width-i%width {
 		text[i%width+1] = 'X'
 		return string(text)
 	}
 	text[i%width+1] = '>'
 	text[width-i%width] = '<'
+
 	return string(text)
 }
