7d16195a39220eddc08b004a7dc148b669bc6efb
Ben Fuller <bfuller@pivotal.io>
2019-10-16 10:11:42 -0600

    fix destroy option
    Signed-off-by: Melena Suliteanu <msuliteanu@pivotal.io>
    Co-authored-by: Melena Suliteanu <msuliteanu@pivotal.io>

--- scripts/smith.sh
+++ scripts/smith.sh
@@ -8,6 +8,6 @@ case "$cmd" in
         curl "https://environments.toolsmiths.cf-app.com/v1/custom_gcp/pks/metadata?api_token=${API_KEY}&name=${2}" | jq .
         ;;
     destroy)
-        curl "https://environments.toolsmiths.cf-app.com/v1/custom_gcp/pks/destroy?api_token=${API_KEY}&name=${2}" | jq .
+        curl "https://environments.toolsmiths.cf-app.com/v1/custom_gcp/pks/destroy" -d "{\"api_token\": \"${API_KEY}\", \"name\": \"${2}\"}" -H "Content-Type: application/json" -v | jq .
         ;;
 esac
