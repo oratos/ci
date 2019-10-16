83c0a562f62511092e9d7e9593463e4c3364b3a1
Melena Suliteanu <msuliteanu@pivotal.io>
2019-10-16 10:04:25 -0600

    add a script to get lock info
    Signed-off-by: Ben Fuller <bfuller@pivotal.io>
    Co-authored-by: Ben Fuller <bfuller@pivotal.io>

---
+++ scripts/smith.sh
@@ -0,0 +1,13 @@
+API_KEY=$(vault read -format json "secret/toolsmith-api-key" | jq .data.key -r )
+cmd=${1:-}
+case "$cmd" in
+    list)
+        curl "https://environments.toolsmiths.cf-app.com/v1/custom_gcp/pks/list?api_token=${API_KEY}" | jq .
+        ;;
+    get-info)
+        curl "https://environments.toolsmiths.cf-app.com/v1/custom_gcp/pks/metadata?api_token=${API_KEY}&name=${2}" | jq .
+        ;;
+    destroy)
+        curl "https://environments.toolsmiths.cf-app.com/v1/custom_gcp/pks/destroy?api_token=${API_KEY}&name=${2}" | jq .
+        ;;
+esac
