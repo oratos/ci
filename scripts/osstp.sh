ec63ce562e003370f66886a37952d99dff90c132
Melena Suliteanu <msuliteanu@pivotal.io>
2019-10-30 11:03:08 -0600

    add spacing to osstp script
    Signed-off-by: Ben Fuller <bfuller@pivotal.io>
    Co-authored-by: Ben Fuller <bfuller@pivotal.io>

--- scripts/osstp.sh
+++ scripts/osstp.sh
@@ -6,11 +6,13 @@ FLUENT_BIT_PLUGIN_REPO=${FLUENT_BIT_PLUGIN_REPO:-~/workspace/fluent-bit-out-sysl
 REMAPPER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/osstp.yaml
 OSSTP_LOAD=${OSSTP_LOAD:-~/workspace/osstpclients/bin/osstp-load.py}
 OSM_API_KEY_FILE=${OSM_API_KEY_FILE:-~/.osmapikey}
+
 pushd $SINK_RESOURCES_REPO
-osstptool generate --download --remapper-definitions-file=$REMAPPER
-python2 $OSSTP_LOAD -R sink-resources-release/latest osstp_golang.yml -I "Distributed - Static Link w/ TP" -A $OSM_API_KEY_FILE
+    osstptool generate --download --remapper-definitions-file=$REMAPPER
+    python2 $OSSTP_LOAD -R sink-resources-release/latest osstp_golang.yml -I "Distributed - Static Link w/ TP" -A $OSM_API_KEY_FILE
 popd
+
 pushd $FLUENT_BIT_PLUGIN_REPO
-osstptool generate --download --remapper-definitions-file=$REMAPPER
-python2 $OSSTP_LOAD -R sink-resources-release/latest osstp_golang.yml -I "Distributed - Static Link w/ TP" -A $OSM_API_KEY_FILE
+    osstptool generate --download --remapper-definitions-file=$REMAPPER
+    python2 $OSSTP_LOAD -R sink-resources-release/latest osstp_golang.yml -I "Distributed - Static Link w/ TP" -A $OSM_API_KEY_FILE
 popd
